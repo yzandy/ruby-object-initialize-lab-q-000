f2525dca01f8fdaf5c12a6681a844d5a630addbc
yzandy <johnandrewnewcomb@gmail.com>
2016-03-13 19:17:10 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+  class Dog
+  def initialize(name, breed="Mut")
+ 	@name = name
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
